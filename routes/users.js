7f495791844a89c83f521c9532a000edabfaae90
Pierre BOUCHER <boucher.pierre44400@hotmail.fr>
2020-02-15 10:12:12 +0100

    le format

--- routes/users.js
+++ routes/users.js
@@ -20,23 +20,6 @@ module.exports = router;
 
 
 var truc = {
-  "fulfillmentText": "This is a text response",
-  "fulfillmentMessages": [
-    {
-      "card": {
-        "title": "card title",
-        "subtitle": "card text",
-        "imageUri": "https://assistant.google.com/static/images/molecule/Molecule-Formation-stop.png",
-        "buttons": [
-          {
-            "text": "button text",
-            "postback": "https://assistant.google.com/"
-          }
-        ]
-      }
-    }
-  ],
-  "source": "example.com",
   "payload": {
     "google": {
       "expectUserResponse": true,
@@ -49,28 +32,7 @@ var truc = {
           }
         ]
       }
-    },
-    "facebook": {
-      "text": "Hello, Facebook!"
-    },
-    "slack": {
-      "text": "This is a text response for Slack."
-    }
-  },
-  "outputContexts": [
-    {
-      "name": "projects/${PROJECT_ID}/agent/sessions/${SESSION_ID}/contexts/context name",
-      "lifespanCount": 5,
-      "parameters": {
-        "param": "param value"
-      }
-    }
-  ],
-  "followupEventInput": {
-    "name": "event name",
-    "languageCode": "en-US",
-    "parameters": {
-      "param": "param value"
     }
   }
 }
+
