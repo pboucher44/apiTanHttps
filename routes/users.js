2483617964be7f993384a15b341d7dead6b0ccf0
Pierre BOUCHER <boucher.pierre44400@hotmail.fr>
2020-02-15 09:58:12 +0100

    bah ouai fallait un post

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.post('/', function(req, res, next) {
 
   res.send(truc);
 });
