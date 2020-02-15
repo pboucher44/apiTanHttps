c2639b4beb39f717dfe611ffb89dc9b3b693509c
Pierre BOUCHER <boucher.pierre44400@hotmail.fr>
2020-02-15 09:10:29 +0100

    Ajout du getter

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,18 @@
 var express = require('express');
+var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.send(httpGet());
 });
+
+function httpGet()
+{
+  var xmlHttp = new XMLHttpRequest();
+  xmlHttp.open( "GET", "http://open.tan.fr/ewp/tempsattente.json/PPDO", false ); // false for synchronous request
+  xmlHttp.send( null );
+  return xmlHttp.responseText;
+}
 
 module.exports = router;
