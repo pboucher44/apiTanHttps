58279d490e0df67a2dadeabfe93279245c61c927
Pierre BOUCHER <boucher.pierre44400@hotmail.fr>
2020-02-15 09:19:10 +0100

    ajout du parse

--- routes/users.js
+++ routes/users.js
@@ -4,7 +4,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send(httpGet());
+  res.send(JSON.parse(httpGet()));
 });
 
 function httpGet()
